af82c70f5872940e35d70e617148992ebdddf829
jmutchek <john5@mutchek.com>
2021-04-11 14:16:37 -0400

    first github release

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,69 +5,21 @@
 #   across multiple profiles and machines.
 
 # To start using on a new machine, first sync your $PROFILE directory to git with
-#   git init
+#   git init -b main
 #   git remote add origin https://github.com/jmutchek/PowerShell.git
 #   git pull origin main
 
-git -C (join-path $HOME "\Documents\PowerShell\") pull
+Write-Host "Using profile https://github.com/jmutchek/PowerShell/commit/$(git -C (join-path $HOME "\Documents\PowerShell\") rev-parse HEAD)"
 
+# Load profile scipt helper functions
 . $(join-path $HOME "\Documents\PowerShell\" "ProfileFunctions.ps1")
 
-$profile_initialized = $false
+# Retrieve latest profile script from github (will be used in the next PowerShell session)
+git -C (join-path $HOME "\Documents\PowerShell\") pull
 
-function Prompt {
+# Set up aliases
+set-alias redo invoke-history
+new-alias z 'D:\local\70-code\73-github-personal\tools\zettelkasten\scripts\z.ps1'
 
-    function Initialize-Profile {
-        
-        if ((Get-Module PowerLine).Version -lt 3.2) {
-            throw "Profile requires PowerLine 3.2+"
-        }
-        
-
-        # Set up environment for johnny.decimal
-        $jd_script = "local\70-code\73-github-personal\johnny.decimal\jd.ps1"
-        $jd_testroots = @( "d:\" )
-
-        ForEach ($root in $jd_testroots) {
-            If (Test-Path -ErrorAction SilentlyContinue (join-path $root $jd_script)) {
-                Write-Host "Setting up jd environment in $root"
-                $Env:jd_roots = $root
-                new-alias jd (join-path $root $jd_script)
-                break
-            } 
-        }
-        $jd_script = $null
-        $jd_testroots = $null
-        
-
-        # Set up the powerline prompt
-        Import-Module PowerLine
-        $Prompt = {}
-        Add-PowerLineBlock { $MyInvocation.HistoryId }
-        Add-PowerLineBlock { "&nbsp;" }
-        Add-PowerLineBlock { Get-JDShortenedPath -SingleCharacterSegment }
-        # white background
-        # Set-PowerLinePrompt -Colors "#ffffff", "#000066" -FullColor -PowerLineFont
-        # black background
-        Set-PowerLinePrompt -Colors "#000000", "#666699" -FullColor -PowerLineFont
-        
-
-        # Set up aliases
-        set-alias redo invoke-history
-        new-alias z 'D:\local\70-code\73-github-personal\tools\zettelkasten\scripts\z.ps1'
-
-
-        $profile_initialized = $true
-        $Prompt
-    }
-    
-    # most initialization is only required for an interactive session
-    if (!$profile_initialized) {
-      Initialize-Profile
-    }
-  
-    $currentLastExitCode = $LASTEXITCODE
-    $lastSuccess = $?
-  
-    $global:LASTEXITCODE = $currentLastExitCode
-  }
+Initialize-JohnnyDecimal @( "d:\" )
+Initialize-Prompt
--- ProfileFunctions.ps1
+++ ProfileFunctions.ps1
@@ -1,3 +1,20 @@
+# Set up environment for johnny.decimal
+function Initialize-JohnnyDecimal($roots) {
+    $jd_script = "local\70-code\73-github-personal\johnny.decimal\jd.ps1"
+    $jd_testroots = $roots
+    ForEach ($root in $jd_testroots) {
+        If (Test-Path -ErrorAction SilentlyContinue (join-path $root $jd_script)) {
+            Write-Host "Setting up jd environment in $root"
+            $Env:jd_roots = $root
+            new-alias jd (join-path $root $jd_script) -Scope Global
+            break
+        } 
+    }
+    $jd_script = $null
+    $jd_testroots = $null
+
+}
+
 function Get-JDShortenedPath {
     [CmdletBinding()]
     param(
@@ -44,6 +61,23 @@ function Get-JDShortenedPath {
     $Path
 }
 
+# Set up the powerline prompt
+function Initialize-Prompt() {
+    Import-Module PowerLine
+    if ((Get-Module PowerLine).Version -lt 3.2) {
+        throw "Profile requires PowerLine 3.2+"
+    }
+
+    $Prompt = {}
+    Add-PowerLineBlock { $MyInvocation.HistoryId }
+    Add-PowerLineBlock { "&nbsp;" }
+    Add-PowerLineBlock { Get-JDShortenedPath -SingleCharacterSegment }
+    # white background
+    # Set-PowerLinePrompt -Colors "#ffffff", "#000066" -FullColor -PowerLineFont
+    # black background
+    Set-PowerLinePrompt -Colors "#000000", "#666699" -FullColor -PowerLineFont   
+}
+
 # Function to set the title of the window
 function Set-Title {
     param(
@@ -51,4 +85,9 @@ function Set-Title {
         $title
     )
     $Host.UI.RawUI.WindowTitle = $title   
+}
+
+# Set up function for 1Password password lookup using jq
+if (get-command -ErrorAction SilentlyContinue op) {
+    function 1p {op get item $args | jq .details.fields}
 }
